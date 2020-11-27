031057b4963da8bb5135837af6493ebf257632e2
Kingston802 <alexkingstonbailey@gmail.com>
2020-11-28 09:15:50 +1300

    fixed footer warning

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import './Footer.css';
 
 const Footer = () => (
@@ -7,9 +6,5 @@ const Footer = () => (
     Website created by Alexander Bailey, <a href="https://www.offmenupodcast.co.uk/">The Off Menu podcast</a> is the property of Plosive limited 
   </div>
 );
-
-Footer.propTypes = {};
-
-Footer.defaultProps = {};
 
 export default Footer;
